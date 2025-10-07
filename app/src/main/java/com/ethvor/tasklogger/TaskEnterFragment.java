7b6254848f5580b3d7f657aa5e54c6dde229da9d
ethvor <contactethanv@gmail.com>
2025-10-07 15:12:07 -0500

    inflated taskenterfragment

--- app/src/main/java/com/ethvor/tasklogger/TaskEnterFragment.java
+++ app/src/main/java/com/ethvor/tasklogger/TaskEnterFragment.java
@@ -3,6 +3,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModel;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class TaskEnterFragment extends Fragment{
     private ViewModel viewModel;
@@ -17,6 +20,11 @@ public class TaskEnterFragment extends Fragment{
         if (getArguments() != null) {
 
         }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_task_enter, container, false);
     }
 
 }
